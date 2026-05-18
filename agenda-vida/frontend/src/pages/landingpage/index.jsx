93eb754acb0ae3a19f16f59fc9bc15beded186d6
Rafudo <rafaplays.018@gmail.com>
2026-05-18 15:40:49 -0300

    landing page

--- agenda-vida/frontend/src/pages/landingpage/index.jsx
+++ agenda-vida/frontend/src/pages/landingpage/index.jsx
@@ -1,10 +1,48 @@
 import Header from "../../components/header/header";
+import "./landingpage.css";
+import Imagem from '../../assets/logo1.png'
+
+
 
 export default function LandingPage(){
     return(
         <>
         <Header />
-        nigga
+        <section className="hero">
+
+    <div className="hero-content">
+
+        <h1>
+            Organize sua rotina com o
+            <span> AgendaVida</span>
+        </h1>
+
+        <p>
+            Controle exames, consultas e agendamentos
+            em um único lugar de forma simples e rápida.
+        </p>
+
+        <div className="hero-buttons">
+
+            <button className="btn-primary">
+                Começar
+            </button>
+
+            <button className="btn-secondary">
+                Saiba Mais
+            </button>
+
+        </div>
+
+    </div>
+
+    <div className="hero-image">
+
+        <img className="imagem" src={Imagem} alt="Mockup do sistema" />
+
+    </div>
+
+</section>
         </>
     )
 }
